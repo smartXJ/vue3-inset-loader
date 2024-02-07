5315a440792b2a6bd8730d4d20e2600c589ab943
xiaojun <xj517356221@163.com>
2024-02-07 16:17:46 +0800

    pref: remove Sourcemap prompt

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * @Author: xiaojun
  * @Date: 2024-02-04 11:49:42
  * @LastEditors: xiaojun
- * @LastEditTime: 2024-02-04 11:55:50
+ * @LastEditTime: 2024-02-07 16:16:13
  * @Description: 对应操作
  */
 // utils.js remains the same
@@ -29,32 +29,30 @@ const myVitePlugin = {
       _init = true;
       init(this);
     }
-    // 如果文件的扩展名不是 .vue，那么我们就跳过这个文件
-    if (!id.endsWith('.vue')) {
-      return code;
-    }
 
-    if (!needHandle) {
-      return code;
-    }
     const route = getRoute(id);
     const curPage = pagesMap[route];
-    if (!curPage) return code
-    const compiler = parse(code);
-    const labelCode = generateLabelCode(curPage.label);
-    let templateCode = compiler.descriptor && compiler.descriptor.template ? compiler.descriptor.template.content : '';
-    templateCode = labelCode + templateCode;
-    const style = generateStyleCode(compiler.descriptor && compiler.descriptor.styles ? compiler.descriptor.styles : []);
-    const script = generateScriptcCode(code);
-  // 重组style标签及内容
-    code = `
-      <template>
-        ${templateCode}
-      </template>
-      ${script}
-      ${style}
-    `;
-    return code;
+    
+    if (id.endsWith('.vue') && needHandle && curPage) {
+      const compiler = parse(code);
+      const labelCode = generateLabelCode(curPage.label);
+      let templateCode = compiler.descriptor && compiler.descriptor.template ? compiler.descriptor.template.content : '';
+      templateCode = labelCode + templateCode;
+      const style = generateStyleCode(compiler.descriptor && compiler.descriptor.styles ? compiler.descriptor.styles : []);
+      const script = generateScriptcCode(code);
+    // 重组style标签及内容
+      code = `
+        <template>
+          ${templateCode}
+        </template>
+        ${script}
+        ${style}
+      `;
+    }
+    return {
+      code,
+      map: null // 表示源码视图不作修改
+    }
   },
 };
 
